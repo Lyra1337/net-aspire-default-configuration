68d995f10c729c0bb937b688152318bfd8f2fcd1
Thomas Leiber <leiber@kk-software.de>
2024-12-04 16:14:12 +0100

    Add project files.

---
+++ AspireConfigurationQuestion.ApiService/SampleService.cs
@@ -0,0 +1,15 @@
+﻿using AspireConfigurationQuestion.ServiceDefaults;
+
+namespace AspireConfigurationQuestion.ApiService
+{
+    public class SampleService(AppConfig appConfig, ILogger<SampleService> logger) : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            logger.LogInformation("AppConfig.One: {One}", appConfig.One);
+            logger.LogInformation("AppConfig.Two: {Two}", appConfig.Two);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ AspireConfigurationQuestion.ServiceDefaults/AppConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace AspireConfigurationQuestion.ServiceDefaults;
+
+public class AppConfig
+{
+    public string? One { get; set; }
+    public string? Two { get; set; }
+}
